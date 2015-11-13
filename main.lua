cb4a0aed8179698c06e4e69e27ec116affdf220b
RickardToren <toren.rickard@gmail.com>
2015-11-13 23:25:45 +0100

    Added camera file.

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 require "planet"
+require "camera"
 
 function love.load()
   love.graphics.setBackgroundColor(0,0,0)
@@ -11,10 +12,12 @@ planet.new(1, 400, 40, 1, 0.02)
 time = 0
 function love.update(dt)
   time = time + dt
+  camera.update()
   planet.update(dt)
 end
 
 function love.draw()
+  camera.draw()
   love.graphics.setColor(255,255,0)
   love.graphics.circle("fill",400,300,100)
   planet.draw()
