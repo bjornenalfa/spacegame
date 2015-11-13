68f99c8a471a605da040b60a38457d9f969cb1f2
RickardToren <toren.rickard@gmail.com>
2015-11-13 23:05:44 +0100

    Added one planet. (Mostly testing).

--- main.lua
+++ main.lua
@@ -4,8 +4,9 @@ function love.load()
   love.graphics.setBackgroundColor(0,0,0)
 end
 
-planet.new(1, 200, 50, 0, 0.1)
-planet.new(1, 300, 20, 2, 0.5)
+planet.new(1, 200, 50, 0, 0.5)
+planet.new(1, 300, 20, 2, 0.1)
+planet.new(1, 400, 40, 1, 0.02)
 
 time = 0
 function love.update(dt)
