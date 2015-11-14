149cece8ce55e27e7297ada0c062e50da74a2f7f
bjornenalfa <bjorn.detterfelt@telia.com>
2015-11-14 18:33:42 +0100

    twin planets :cool:

--- main.lua
+++ main.lua
@@ -7,14 +7,17 @@ require "player"
 require "background"
 
 function love.load()
-  projectile.new(image.missile_atomicbomb, 1, -360, 250, 20, 20, 200, 0, 90)
+  --projectile.new(image.missile_atomicbomb, 1, -360, 250, 20, 20, 200, 0, 90)
   
   -- suns and planets
   -- mass, x, y, radius, color, image
-  sun = planet.newSun(1e8, 0, 0, 150, {255, 255, 255}, image.sun)
+  sun = planet.newSun(1e8, 0, 0, 100, {255, 255, 255}, image.sun)
   -- mass, distance, radius, startAngle, speed, health, color, image
-  planet1 = planet.new( 20e6,   450,  50,   0,    0.5,    100,  {200, 0, 200},    image.planet_1)
-  planet2 = planet.new( 20e6,   450,  50,   180,    0.5,    100,  {200, 0, 200},    image.planet_1)
+  planetDuoBase = planet.new( 1,   150,  0,   270,  0.5,   100000,  {0, 0, 0},  image.planet_1)
+  planetDuo1 = planet.new( 4e6,   30,  20,   180,  1,   100,  {255, 100, 100},  image.planet_1)
+  planetDuo2 = planet.new( 4e6,   30,  20,   0,  1,   100,  {100, 100, 255},  image.planet_1)
+  planet1 = planet.new( 20e6,   350,  50,   0,    0.5,    100,  {200, 0, 200},    image.planet_1)
+  planet2 = planet.new( 20e6,   350,  50,   180,    0.5,    100,  {200, 0, 200},    image.planet_1)
   planet3 = planet.new( 8e6,    270,  20,   90,   1,      100,  {0, 0, 250},      image.planet_1)
   planet4 = planet.new( 15e6,   800,  40,   180,  0.15,   100,  {100, 100, 100},  image.planet_1)
   moon1 = planet.new(   1e6,    100,  10,   0,    1.5,    100,  {200, 0, 200},    image.planet_1)
@@ -23,6 +26,9 @@ function love.load()
   sun:addMoon(planet2)
   sun:addMoon(planet3)
   sun:addMoon(planet4)
+  sun:addMoon(planetDuoBase)
+  planetDuoBase:addMoon(planetDuo1)
+  planetDuoBase:addMoon(planetDuo2)
   planet1:addMoon(moon1)
   planet1:addMoon(moon2)
   
