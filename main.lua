388648c393cbbcd9d6fd9f328fb8888bc6e0ecce
RickardToren <toren.rickard@gmail.com>
2015-11-14 12:59:27 +0100

    Nerfed all planets and projectiles.

--- main.lua
+++ main.lua
@@ -5,16 +5,16 @@ require "image"
 require "player"
 
 function love.load()
-  projectile.new(image.missile_atomicbomb, 0.01, -360, 250, 20, 20, 200, 0, 90)
+  projectile.new(image.missile_atomicbomb, 1, -360, 250, 20, 20, 200, 0, 90)
   
   -- suns and planets
   -- mass, x, y, radius, color, image
-  sun = planet.newSun(100, 0, 0, 200, {255, 255, 255}, image.sun)
+  sun = planet.newSun(100, 0, 0, 150, {255, 255, 255}, image.sun)
   -- mass, distance, radius, angle, speed, health, color, image
-  planet1 = planet.new(1, 200, 50, 0, 0.5, 100, {200, 0, 200}, image.planet_1)
-  planet2 = planet.new(1, 300, 20, 2, 0.1, 100, {0, 0, 250}, image.planet_1)
-  planet3 = planet.new(1, 400, 40, 1, 0.02, 100, {100, 100, 100}, image.planet_1)
-  moon1 = planet.new(1, 150, 10, 0, 1.5, 100, {200, 0, 200}, image.planet_1)
+  planet1 = planet.new(20, 450, 50, 0, 0.5, 100, {200, 0, 200}, image.planet_1)
+  planet2 = planet.new(8, 270, 20, 2, 1, 100, {0, 0, 250}, image.planet_1)
+  planet3 = planet.new(15, 800, 40, 1, 0.15, 100, {100, 100, 100}, image.planet_1)
+  moon1 = planet.new(1, 100, 10, 0, 1.5, 100, {200, 0, 200}, image.planet_1)
   sun:addMoon(planet1)
   sun:addMoon(planet2)
   sun:addMoon(planet3)
