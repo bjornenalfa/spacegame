f9cad17e45ed60ad256cdc2405fce59803119c05
bjornenalfa <bjorn.detterfelt@telia.com>
2015-11-14 13:01:03 +0100

    projectile gravity

--- projectile.lua
+++ projectile.lua
@@ -20,8 +20,20 @@ end
 function projectile.update(dt)
   collisions = {}
   for projectileIndex, pr in pairs(p.projectiles) do
+    ax = 0
+    ay = 0
+    for _, pl in pairs(planet.planets) do
+      sqdis = (pl.x - pr.x)*(pl.x - pr.x) + (pl.y - pr.y)*(pl.y - pr.y)
+      angle = math.atan2(pr.y - pl.y, pr.x - pl.x)
+      g = (pr.m * pl.m) / sqdis
+      ax = ax + math.cos(angle) * g
+      ay = ay + math.sin(angle) * g
+    end
+    pr.vx = pr.vx + ax * dt 
+    pr.vy = pr.vx + ay * dt
     pr.x = pr.x + (pr.vx * dt)
     pr.y = pr.y + (pr.vy * dt)
+    
     for _, pl in pairs(planet.planets) do
       if math.sqrt((pl.x - pr.x)*(pl.x - pr.x) + (pl.y - pr.y)*(pl.y - pr.y)) < pl.r then
         if not pl.isSun then
