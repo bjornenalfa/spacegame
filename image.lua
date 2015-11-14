f0a83ecb1c94f66e5581152a891a10d0625e8894
bjornenalfa <bjorn.detterfelt@telia.com>
2015-11-14 11:43:51 +0100

    easy image adding

--- image.lua
+++ image.lua
@@ -1,12 +1,10 @@
 image = {}
 local i = image
 
-i.sun = love.graphics.newImage("media/sun.png")
 
-
-i.planet = {}
-function addPlanetImage(name)
-  table.insert(i.planet, love.graphics.newImage("media/"..name..".png"))
+function addImage(name)
+  table.insert(i, name, love.graphics.newImage("media/"..name..".png"))
 end
 
-addPlanetImage("planet_1")
+addImage("sun")
+addImage("planet_1")
--- main.lua
+++ main.lua
@@ -7,10 +7,10 @@ function love.load()
   -- mass, x, y, radius, color, image
   sun = planet.newSun(100, 0, 0, 200, {255, 255, 255}, image.sun)
   -- mass, distance, radius, angle, speed, health, color, image
-  sun:addMoon(planet.new(1, 300, 20, 2, 0.1, 100, {0, 0, 250}, image.planet[1]))
-  sun:addMoon(planet.new(1, 400, 40, 1, 0.02, 100, {100, 100, 100}, image.planet[1]))
-  planet1 = planet.new(1, 200, 50, 0, 0.5, 100, {200, 0, 200}, image.planet[1])
-  moon1 = planet.new(1, 150, 10, 0, 1.5, 100, {200, 0, 200}, image.planet[1])
+  sun:addMoon(planet.new(1, 300, 20, 2, 0.1, 100, {0, 0, 250}, image.planet_1))
+  sun:addMoon(planet.new(1, 400, 40, 1, 0.02, 100, {100, 100, 100}, image.planet_1))
+  planet1 = planet.new(1, 200, 50, 0, 0.5, 100, {200, 0, 200}, image.planet_1)
+  moon1 = planet.new(1, 150, 10, 0, 1.5, 100, {200, 0, 200}, image.planet_1)
   sun:addMoon(planet1)
   planet1:addMoon(moon1)
 end
