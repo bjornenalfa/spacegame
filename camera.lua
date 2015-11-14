f2d67b5b62747896953a7c003fe21daa86d3ffb2
RickardToren <toren.rickard@gmail.com>
2015-11-14 12:49:08 +0100

    Added some zoom.

--- camera.lua
+++ camera.lua
@@ -8,10 +8,12 @@ c.y = 0
 
 function camera.update()
   c.x, c.y = planet.getPlayerOne()
-  c.x = c.x + c.width/2
-  c.y = c.y + c.height/2
+  -- bad zoom
+  c.x = c.x * 0.3 + c.width/2
+  c.y = c.y * 0.3 + c.height/2
 end
 
 function camera.draw()
   love.graphics.translate(c.x, c.y)
+  love.graphics.scale(0.3)
 end
