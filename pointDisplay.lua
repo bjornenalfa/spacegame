cec6f44508c4ce76031950c383ce60d9664807d6
Pjoelj <pjoelj1@gmail.com>
2015-11-15 12:30:41 +0100

    Poängdisplay tillagd
    Inte alltför snygg, men den fungerar.

---
+++ pointDisplay.lua
@@ -0,0 +1,10 @@
+pointDisplay = {}
+local p = pointDisplay
+
+function pointDisplay.draw()
+  for i = 1, #player.players, 1 do
+    love.graphics.setFont(font.base)
+    love.graphics.setColor({255,255,255})
+    love.graphics.printf(player.players[i].name .. ": " .. player.players[i].score, -love.graphics.getWidth()/2, -love.graphics.getHeight()/2 + 100 * i, 200000, "left")
+  end
+end
